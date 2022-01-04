74fd5e7405df93b9e7ca34eff8fae937c2c46e94
Christopher <chrisotazo77@gmail.com>
2022-01-04 11:14:43 -0300

    Pendiente

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const meals = require ('./routes/meals')
+const orders = require ('./routes/orders')
+const auth = require ('./routes/auth')
+const cors = require ('cors')
+
+const app = express()
+app.use(bodyParser.json())
+ 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use('/api/meals', meals)
+app.use('/api/orders', orders)
+app.use('/api/auth', auth)
+module.exports = app
